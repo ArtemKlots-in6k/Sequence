17da8cff3a0f26a7a059cfea2a56b54d50209419
Artem Klots <ArtemKl@interlink-ua.com>
2016-07-08 15:08:39 +0300

    Checking for null

--- src/Sequence.java
+++ src/Sequence.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * Created by employee on 7/8/16.
  */
 public class Sequence {
+
+    public ArrayList<Integer> findSequence() {
+        return new ArrayList<Integer>();
+    }
 }
--- tests/SequenceTests.java
+++ tests/SequenceTests.java
@@ -1,5 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by employee on 7/8/16.
  */
 public class SequenceTests {
+    private Sequence sequence;
+
+    @Before
+    public void createSequence(){
+        sequence = new Sequence();
+    }
+
+    @Test
+    public void checkingForNull() throws Exception {
+        assertThat(sequence.findSequence(), is(new ArrayList<Integer>()));
+    }
+
+
 }
